24d2c6edc52771dad6d72f07d425763e170ea8c1
varunkamathi <varunkamathi522004@gmail.com>
2024-08-30 14:56:53 +0530

    header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,11 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 export default function App() {
   return <BrowserRouter>
+  {/* header*/}
+  <Header />
      <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
