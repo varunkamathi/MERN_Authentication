c53eebcfac4bfb803039d397ce4391fab57932d4
varunkamathi <varunkamathi522004@gmail.com>
2024-08-31 19:48:50 +0530

    test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongooes from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongooes.connect(process.env.MONGO).then(()=>{
@@ -14,3 +15,6 @@ const app = express();
 app.listen(3000 ,()  =>{
 console.log("Server listening on port 3000")
 });
+
+//api
+app.use("/api/user", userRoutes);
