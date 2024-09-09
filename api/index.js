b053cc755b2fe6ff0e0ec0d1df855819da137515
varunkamathi <varunkamathi522004@gmail.com>
2024-09-09 11:26:56 +0530

    add middleWare

--- api/index.js
+++ api/index.js
@@ -23,3 +23,15 @@ console.log("Server listening on port 3000")
 //api
 app.use("/api/user", userRoutes);
 app.use("/api/auth" , authRoutes);
+
+
+app.use((err, req, res, next)=> {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+       });
+         
+});
