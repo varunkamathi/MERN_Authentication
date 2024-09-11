0e42538a76c0f76c5cd070c2d587eae931b3eb33
varunkamathi <varunkamathi522004@gmail.com>
2024-09-11 11:31:24 +0530

    add signup functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 export default function Signup() {
+  const [fromData, setFromData] = useState({});
+  const handleChange = (e) => {
+    setFromData({...fromData, [e.target.id]: e.target.value});
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefult();
+    const res = await fetch('/api/auth/signup',{
+      method: 'POST',
+      headers:{
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(fromData),
+    });
+    const data = await res.json();
+    console.log(data); {message : 'User created successfully'}
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg'/>
-        <input type= "email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
-        <input type= "password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
+      <h1  className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg'onChange={handleChange}/>
+        <input type= "email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+        <input type= "password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
         <button className='bg-black text-cyan-50 rounded-xl p-3 uppercase hover:opacity-80'>sign up</button>
         
       </form>
