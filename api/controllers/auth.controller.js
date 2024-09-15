7e79f2fe1936da268ea4b7cf61cdcb642ef1672a
varunkamathi <varunkamathi522004@gmail.com>
2024-09-15 11:54:00 +0530

    signin api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import {errorHandler} from '../util/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res, next) => {
     const { username, email, password} = req.body;
@@ -12,3 +14,17 @@ export const signup = async(req, res, next) => {
 
     }
 };
+
+export const signin = async(req, res, next) =>{
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email})
+        if (!validUser) return next(errorHandler(404, 'User Not Found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);    
+        if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)  ;
+        res.cookie('access_token', token, {httpOnly: true }).status(200).json(validUser); 
+    } catch (error) {
+        next(error)
+    }
+}
