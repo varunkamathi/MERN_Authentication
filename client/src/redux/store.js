1958c1416395a5e188ecd3251cc5638a622c87eb
varunkamathi <varunkamathi522004@gmail.com>
2024-09-19 10:30:05 +0530

    redux tool kit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {user : userReducer},
  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
+
+export default store;
+
