7076606d948e560a9204a17ed8bb2bfb1f2f9b87
varunkamathi <varunkamathi522004@gmail.com>
2024-09-20 11:46:04 +0530

    oAuth frontendcomponents

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_bhZJ3tSpDLMtG48rKxW04bUc0Hls2M",
+  authDomain: "mern-auth-3339f.firebaseapp.com",
+  projectId: "mern-auth-3339f",
+  storageBucket: "mern-auth-3339f.appspot.com",
+  messagingSenderId: "635529452790",
+  appId: "1:635529452790:web:5b22a94547487ffd6220d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -60,6 +61,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don't Have an account?</p>
